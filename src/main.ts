6ef9fd470c111bc06b695ff3f2b23dbe1c74403d
Juan David Reina <judareiro@gmail.com>
2025-05-14 00:48:31 -0500

    fixing the login route in prod

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  console.log(`Port: ${port}`);
+  console.log(`Global prefix: api/v1`);
 }
 bootstrap();
